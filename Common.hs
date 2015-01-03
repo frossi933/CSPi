829a871af915e6059a8428d05f29c19f89f5ca69
Federico Rossi <frossi.933@gmail.com>
2015-01-03 21:10:41 +0000

    modificacion implementacion de Procesos

--- Common.hs
+++ Common.hs
@@ -1,23 +1,40 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module Common where
 
-    type Func = () -- IO ()
+    import Data.List
+--    data Func where
+--        F :: Monad m => m () -> Func
+    type Func = IO ()    
     data Event = Out String Bool | In String Func
 
-    data Id a = Id a deriving(Show)
-    newtype State s a = State { runState :: s -> (a,s) }
-
-    instance Monad Id where
-        return x = Id x
-        Id x >>= f = f x
-
-    instance Monad (State s) where
-        return x = State $ \s -> (x,s)
-        (State h) >>= f = State $ \s -> let (a, newState) = h s
-                                            (State g) = f a
-                                        in  g newState
-
-    instance Ord Event where
-        compare (_ s _) (_ s' _) = compare s s'
-                                        
+    instance Eq Event where
+        (==) e e' = strOf e == strOf e'
+        
+    
     data ProcDef = ProcDef String Proc
-    data Proc = Skip | Stop | Ref String | Prefix Event Proc | Parallel [Proc] | ExtSel [Proc]
+--    data Proc = Skip | Stop | Ref String | Prefix Event Proc | Parallel [Proc] | ExtSel [Proc]
+    data Proc = Skip | Stop | P (Event -> Proc)
+    
+    
+    strOf :: Event -> String
+    strOf (Out s _) = s
+    strOf (In s _) = s
+    
+    skip :: Proc
+    skip = choice [] -- por ahora
+    
+    stop :: Proc
+    stop = choice []
+    
+    prefix :: Event ->  Proc -> Proc
+    prefix e p = choice [(e,p)]
+                                 
+    choice :: [(Event,Proc)] -> Proc
+    choice [] = Stop
+    choice (x:xs) = if chkEv (x:xs) then P (\e' -> maybe Stop snd (find (\(ee,pp) -> e' == ee) (x:xs)))
+                                    else Stop
+                                            where chkEv [] = True
+                                                  chkEv ((e, p):xs) = case find (\(e', p') -> e == e') xs of
+                                                                                    Just _-> False
+                                                                                    Nothing -> chkEv xs
